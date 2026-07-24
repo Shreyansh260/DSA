5cffa85082217964e57b500afd4b6aa9063aa88b
Shreyansh <shreyanshsinghrathore7@gmail.com>
2026-07-24 23:03:31 +0530

    Due to some issue all the previous commits are not tracked so for now this issue is solved

--- Basics/Ternary_Conditions/Ternary.java
+++ Basics/Ternary_Conditions/Ternary.java
@@ -4,6 +4,7 @@ public class Ternary {
     public static void main(String[]args ){
         int a = 10;
         int b = 20;
+        //Variable = (condition) ? expressionTrue : expressionFalse;
         int max = (a>b) ? a : b;
         System.out.println("The maximum value is: " + max);
     }
--- Basics/Ternary_Conditions/Ternery.py
+++ Basics/Ternary_Conditions/Ternery.py
@@ -1,3 +1,4 @@
 a = 10
 b = 30
+# Expression1 if condition is true else Expression2
 print("a is greater than b" if (a > b) else "b is greater than a")
